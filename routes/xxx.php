17becf8385ef2ea86b8957a039be3bc911ed5bbe
Fukuda Junjio <tksmjf@icloud.com>
2019-07-18 12:37:46 +0000

    課題できました

--- routes/xxx.php
+++ routes/xxx.php
@@ -18,3 +18,7 @@ Route::get('/xxx', 'AAAController@bbb');
 の add Action に、admin/profile/edit にアクセスしたら ProfileController のedit Actionに
 割り当てるように設定してください。*/
 
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\profileController@edit');
+});
