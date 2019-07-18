391ba795d0e398d13fa823f2bf0b3f46d71aa120
Fukuda Junjio <tksmjf@icloud.com>
2019-07-18 13:54:03 +0000

    課題やりました

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="view-port" content="width=device-width, initial-scale=1">
+        
+        <titlw>Profile</titlw>
+    </head>
+    <body>
+    <h1>Profile作成画面</h1>
+    <h2>課題1.Viewは何をするところでしょうか。簡潔に説明してみてください。</h2>
+    <p>Controllerの指示によって、アクセスしてきたuserのブラウザに表示するデータを生成する所</p>
+    <h3>課題2.プログラマーがhtmlを書かずにPHPなどのプログラミング言語やフレームワークを使う必要があるのはどういった理由でしょうか。</h3>
+    <p>loginしたuser毎にwebpageにuser名を表示したい場合やmodel経由でdatebaseからdateを取得し、それをhtmlファイルに記載してuserに渡す必要があるため</p>
+    </body>
+</html>
