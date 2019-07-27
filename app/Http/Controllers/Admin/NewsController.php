035e3afa424a214c574e7ecfcb880427e9ee0368
Fukuda Junjio <tksmjf@icloud.com>
2019-07-27 11:15:08 +0000

    課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -1,13 +1,13 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\News;
+
 class NewsController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.news.create');
@@ -16,8 +16,47 @@ class NewsController extends Controller
     // 以下を追記
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        
+        $news = new News;
+        $from = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['images'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['_image']);
+        
+         // データベースに保存する
+         $news->fill('form');
+         $news->save();
+        
     // admin/news/createにリダイレクトする
     return redirect('admin/news/create');
     }
+
+
+     public function index(Request $request)
+     {
+         $cond_title = $request->cond_title;
+         if($cond_title != '') {
+             // 検索されたら検索結果を取得する
+             $posts = News::where('title', $cond_title)->get();
+         } else {
+             // それ以外はすべてのニュースを取得する
+             $posts = News::all();
+         }
+         return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+     }
 }
 
+/*
+課題1. 
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::post('news/create', 'Admin\NewsController@create');
      Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
      Route::post('profile/create', 'Admin\ProfileController@create');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 
@@ -31,3 +32,26 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+課題1. GET/POSTメソッドについて調べ、どのような違いがあるか説明してください。
+getメソッドは、ブラウザにURLを入力しURL内の内容を取り出すための要求。
+postメソッドは、URLに対して内容を取り出すだけでなく、クライアントから様々なデータを
+送信できる。主にデータを更新するような時に使われる。
+
+課題2. GET/POSTメソッド以外にどのようなメソッドがあるか、またどのように使われるかを
+       説明してください。
+PUT　・・・　（データの更新に使用）
+PATCH　・・・　（ほぼPUTと同じですが、ごく一部を更新）
+DELETE　・・・　（データの削除に使用）
+OPTIONS　・・・　（使えるメソッド一覧を表示)
+
+課題3. routes/web.php を編集して、 admin/profile/create に postメソッドでアクセスしたら ProfileController の
+       create Action に割り当てるように設定してください。
+上記記載
+
+課題4. resources/views/admin/profile/create.blade.php を編集して、氏名(name)、
+       性別(gender)、趣味(hobby)、自己紹介欄(introduction)を入力するフォームを
+       作成してください。また、formの送信先(<form action=”この部分”>)を、
+       Admin\ProfileController の create Action に指定してください。
+       (ヒント: resources/views/admin/news/create.blade.php)
