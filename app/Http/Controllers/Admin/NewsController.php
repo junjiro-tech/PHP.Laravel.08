b589e43eba66b47ca52cf77bf44f729b73e80ae9
Fukuda Junjio <tksmjf@icloud.com>
2019-08-22 11:09:07 +0000

    hh

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -28,7 +28,7 @@ class NewsController extends Controller
         
         // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
         if (isset($form['images'])) {
-        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
         $news->image_path = Storage::disk('s3')->url($path);
         } else {
         $news->image_path = null;
@@ -86,7 +86,7 @@ class NewsController extends Controller
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $path = Storage::disk('s3')->putFile('/',$news_form['image'],'public');
             $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
